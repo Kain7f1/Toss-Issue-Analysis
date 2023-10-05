428f3437200b7d538cdf8a18532788c1f805ff8c
Kain7f1 <kain7f1@gmail.com>
2023-10-05 16:36:19 +0900

    Complete dcinside_url_crawler.py
    [modify]
    - dcinside_url_crawler.py
    - 모든 갤러리, 모든 검색어를 대상으로 검색할 수 있도록 하였다
    - utility_module.py 도 수정함

--- crawling/src/dcinside_content_crawler.py
+++ crawling/src/dcinside_content_crawler.py
@@ -3,7 +3,7 @@
 # 20230927
 #############################
 
-import utility_module as utility
+import utility_module as util
 import requests
 import pandas as pd
 import re
@@ -49,7 +49,7 @@ def get_content_dc(media):
     }
     url_folder_path = f"./url/{media}"              # 읽어올 폴더 경로 설정
     content_folder_path = f"./content/{media}"      # 저장할 폴더 경로 설정
-    utility.create_folder(content_folder_path)      # 저장할 폴더 만들기
+    util.create_folder(content_folder_path)      # 저장할 폴더 만들기
     error_log = []                                  # 에러 로그 저장 [’error’]
     data_list = []                                  # 데이터 리스트 ['date', 'title', 'url', 'media', 'content', 'is_comment']
     url_csv_file_name = f"url_{media}.csv"          # url 파일 이름
@@ -66,7 +66,7 @@ def get_content_dc(media):
     # 4) 끝나면 파일로 저장, 에러로그 체크
     #################################
     # 1) url.csv를 df로 읽어옴
-    df_url = utility.read_file(url_folder_path, url_csv_file_name)
+    df_url = util.read_file(url_folder_path, url_csv_file_name)
     # 게시글 하나씩 읽어옴 ['date', 'title', 'url', 'media']
     for index, row in df_url.iterrows():
         # row는 ['date', 'title', 'url', 'media'] 형태
@@ -82,10 +82,10 @@ def get_content_dc(media):
         try:
             response = requests.get(url, headers=header)
             soup = BeautifulSoup(response.text, "html.parser")
-            content = utility.preprocess_content(soup.find('div', {"class": "write_div"}).text)  # 전처리
+            content = util.preprocess_content_dc(soup.find('div', {"class": "write_div"}).text)  # 전처리
             content = title + " " + content      # 본문이 짧을 때는 제목에 메세지가 담겨있는 경우가 많아서 이렇게 처리함
             new_row = [row['date'], title, url, media, content, 0]  # new_row에 정보를 채워둔다
-            data_list.append(new_row) # data_list에 new_row를 추가한다
+            data_list.append(new_row)   # data_list에 new_row를 추가한다
             print(new_row)
         except Exception as e:
             step = "[본문 정보 row를 data_list에 추가]"
@@ -121,6 +121,9 @@ def get_content_dc(media):
                 if reply.find('span', {'data-nick': '댓글돌이'}):
                     print("[댓글돌이는 무시합니다]")
                     continue
+                if reply.find('div', {'class': 'comment_dccon'}):
+                    print("[디시콘은 무시합니다]")
+                    continue
                 # 댓글 정보 가져오기 : date, content
                 date = reply.find("span", {"class": "date_time"}).text.replace(".", "-")  # 댓글 등록 날짜 추출
                 if date[:2] == "20":    # 작년 이전은 "2022.09.07 10:10:54" 형식임
@@ -128,7 +131,7 @@ def get_content_dc(media):
                 else:                   # 올해는 "09.30 10:10:54" 형식임
                     date = now_year + "-" + date[:5]
                 content = reply.find("p", {"class": "usertxt ub-word"}).text  # 댓글 내용 추출
-                content = utility.preprocess_content(content)
+                content = util.preprocess_content_dc(content)    # 전처리
                 new_row = [date, title, url, media, content, 1]  # new_row에 정보를 채워둔다
                 data_list.append(new_row)                        # data_list에 new_row를 추가한다
                 print(new_row)
@@ -142,12 +145,12 @@ def get_content_dc(media):
     # 4-a) 결과 csv 파일로 저장
     try:
         df_result = pd.DataFrame(data_list, columns=['date', 'title', 'url', 'media', 'content', 'is_comment'])
-        utility.save_file(df_result, content_folder_path, f"{content_csv_file_name}.csv")
+        util.save_file(df_result, content_folder_path, f"{content_csv_file_name}.csv")
     except Exception as e:
         print('[error][결과 csv 파일로 저장] ', e)
 
     # 4-b) 에러 로그 체크
     try:
-        utility.error_check(error_log, content_folder_path, content_csv_file_name)
+        util.error_check(error_log, content_folder_path, content_csv_file_name)
     except Exception as e:
         print('[error][에러 로그 체크] ', e)
--- crawling/src/utility_module.py
+++ crawling/src/utility_module.py
@@ -1,40 +1,18 @@
-from calendar import monthrange
-from datetime import datetime
 import pandas as pd
 import os
 import re
 
 
-# 입력 : 2000.01.01 형식을 입력받아서
-# return : 그 달의 마지막 날짜를 리턴
-def get_last_date(date_string):
-    date_format = "%Y.%m.%d"    # 날짜 형식 (예: 2023.01.01)
-    date_object = datetime.strptime(date_string, date_format)
-    _, last_day = monthrange(date_object.year, date_object.month)
-    return f"{date_object.year}.{date_object.month:02d}.{last_day:02d}"
-
-
-def make_date_range(year, month):
-    last_date = get_last_date(f'{year}.{month}.01')
-    date_list = []
-    for day in range(1, 30, 3):
-        day_to = day + 2
-        if day == 28:
-            day_to = int(last_date[-2:])
-        date_list.append([f'{year}.{month}.{day:02d}', f'{year}.{month}.{day_to:02d}'])
-    return date_list
-
-
-# 기능 : 폴더를  생성한다
+# 기능 : 폴더를 생성한다
 # 입력값 : 파일 경로(이름)
 def create_folder(folder_path):
     # 폴더가 존재하지 않는 경우, 폴더 생성
     if os.path.exists(folder_path):
-        print("폴더가 이미 존재합니다.")
+        print("[폴더가 이미 존재합니다]")
     else:
         os.makedirs(folder_path)
-        print(f"폴더 : {folder_path}를 만들었습니다.")
-    print("create_folder()를 종료합니다")
+        print(f"[폴더 : {folder_path}를 만들었습니다]")
+    print("[create_folder()를 종료합니다]")
 
 
 # 입력값 : folder_path, endswith (파일이름의 검색조건 : 파일명의 끝)
@@ -42,7 +20,7 @@ def create_folder(folder_path):
 def load_file_names(folder_path, endswith='url.csv'):
     files = []
     # 폴더 내의 모든 파일을 탐색
-    print(f"load_files() : {folder_path} 내의 파일을 탐색합니다. 검색조건 : endswith={endswith}")
+    print(f"[{folder_path} 내의 파일을 탐색합니다. 검색조건 : endswith={endswith}]")
     for filename in os.listdir(folder_path):
         if filename.endswith(endswith):
             files.append(filename)
@@ -67,30 +45,11 @@ def save_file(df, folder_path, file_name):
     df.to_csv(combined_path, encoding='utf-8', index=False)     # df의 내용을 csv 형식으로 저장합니다
 
 
-def preprocess_content(input_str):
-    return re.sub(r'[^\w\s.]|\d', ' ', input_str)
-
-
-def preprocess_file(keyword):
-    folder_path = f"./{keyword}"
-    content_file_names = load_file_names(folder_path, endswith='content.csv')
-    print(f'preprocess_file({keyword})를 진행합니다')
-    for content_file_name in content_file_names:
-        preprocessed_file_name = f"{content_file_name[:-4]}_preprocessed.csv"   # 저장할 파일 이름
-        df = read_file(folder_path, file_name=content_file_name)    # 파일 읽어오기
-        df['content'] = df['content'].astype(str)
-        df['content'] = df['content'].apply(preprocess_content)     # 전처리
-        df = df.loc[df['content'].shift() != df['content']]         # 중복되는 행 지우기
-        # csv로 만든다
-        save_file(df, folder_path, file_name=preprocessed_file_name)
-        print(f"[종료] : {content_file_name} 파일의 content로부터 {preprocessed_file_name} 파일을 만들었습니다")
-
-
 def combine_content_file(keyword):
     content_file_names = load_file_names(f"./{keyword}/content", endswith='content.csv')
     result_file_name = f"{keyword}_content.csv"
     dataframes = []
-    print(f'combine_content_file({keyword})를 진행합니다')
+    print(f'[combine_content_file({keyword})를 진행합니다]')
     for content_file_name in content_file_names:
         df_content = read_file(f"./{keyword}/content", file_name=content_file_name)
         dataframes.append(df_content)
@@ -100,42 +59,7 @@ def combine_content_file(keyword):
     merged_df.dropna(inplace=True)
     # csv로 만든다
     save_file(merged_df, f"./{keyword}", file_name=result_file_name)
-    print(f"[종료] content 파일들을 하나로 합쳤습니다.")
-
-
-def is_korean(s):
-    return bool(re.fullmatch("[\u3131-\u3163\uAC00-\uD7A3]+", s))
-
-
-# input_list의 원소에 criteria 의 원소 중 하나라도 있으면 True, 아니면 False
-def check_criteria(input_list, criteria):
-    return any(element in input_list for element in criteria)
-
-
-def remove_stopword(keyword):
-    # 파일 열기
-    folder_path = f'./{keyword}'
-    file_name = f'{keyword}_content_tokenized.csv'
-    with open('stopwords.txt', 'r', encoding='utf-8') as file:
-        # 라인별로 읽어 리스트로 저장
-        stopwords = file.readlines()
-    stopwords = [word.strip() for word in stopwords]
-    print('stopwords.txt 를 불러왔습니다')
-
-    df = read_file(folder_path, file_name)
-    print(f'{file_name} 를 불러왔습니다')
-    for stopword in stopwords:
-        df['content'] = df['content'].str.replace(f'\\b{stopword} \\b', '', regex=True)
-    print('stopwords를 지웠습니다')
-
-    save_file(df, folder_path, f'{keyword}_content_tokenized_removed_stopwords.csv')
-    print(f'{keyword}_removed_stopwords.csv 파일을 저장했습니다')
-
-
-def dropna_and_save(file_path):
-    df = pd.read_csv(file_path, encoding='utf-8')
-    df.dropna(inplace=True)
-    df.to_csv(file_path, encoding='utf-8', index=False)
+    print(f"[content 파일들을 하나로 합쳤습니다]")
 
 
 # 목적 : 에러 로그를 읽고, 에러가 있으면 csv 파일을 만든다
@@ -162,10 +86,56 @@ def preprocess_title(text):
     else:
         return result
 
-def preprocess_content(text):
+
+def preprocess_content_dc(text):
     result = text.replace("- dc official App", "").replace("- dc App", "").replace(',', '').strip()
     if len(result) == 0:
         return "_"
     else:
         return result
 
+
+# input_list의 원소에 blacklist 의 원소 중 하나라도 있으면 True, 아니면 False
+def is_in_blacklist(input_list, blacklist):
+    return any(element in input_list for element in blacklist)
+
+
+#########################################################################################################
+# 한국어인지 검사하는 함수
+def is_korean(s):
+    return bool(re.fullmatch("[\u3131-\u3163\uAC00-\uD7A3]+", s))
+
+
+# 기능 : 입력한 문자열에, blacklist의 원소가 포함되어 있으면 True, 아니면 False를 리턴하는 함수
+def contains_blacklist(str_, blacklist):
+    for item in blacklist:
+        if item in str_:
+            return True
+    return False
+
+
+# 불용어를 지운다
+def remove_stopword(keyword):
+    # 파일 열기
+    folder_path = f'./{keyword}'
+    file_name = f'{keyword}_content_tokenized.csv'
+    with open('stopwords.txt', 'r', encoding='utf-8') as file:
+        # 라인별로 읽어 리스트로 저장
+        stopwords = file.readlines()
+    stopwords = [word.strip() for word in stopwords]
+    print('stopwords.txt 를 불러왔습니다')
+
+    df = read_file(folder_path, file_name)
+    print(f'{file_name} 를 불러왔습니다')
+    for stopword in stopwords:
+        df['content'] = df['content'].str.replace(f'\\b{stopword} \\b', '', regex=True)
+    print('stopwords를 지웠습니다')
+
+    save_file(df, folder_path, f'{keyword}_content_tokenized_removed_stopwords.csv')
+    print(f'{keyword}_removed_stopwords.csv 파일을 저장했습니다')
+
+
+def dropna_and_save(file_path):
+    df = pd.read_csv(file_path, encoding='utf-8')
+    df.dropna(inplace=True)
+    df.to_csv(file_path, encoding='utf-8', index=False)
