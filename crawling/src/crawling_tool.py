0cb953620958cbff701e10fa3d6fbf5fab293c70
Kain7f1 <kain7f1@gmail.com>
2023-10-09 13:07:09 +0900

    get_search_result() 결과가 안나오는 경우 보완
    - get_search_result() 결과가 안나오는 경우가 있는데, 값을 불러오지 못해 list의 길이가 0이다. 광고글을 포함해 최소 1개 이상이어야 하는데 말이다. 그래서 0개일 경우 재귀해준다.

--- crawling/src/crawling_tool.py
+++ crawling/src/crawling_tool.py
@@ -9,18 +9,18 @@ import utility_module as util
 # dcinside 봇 차단을 위한 헤더 설정
 header_dc = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
-        "Connection" : "keep-alive",
-        "Cache-Control" : "max-age=0",
-        "sec-ch-ua-mobile" : "?0",
-        "DNT" : "1",
-        "Upgrade-Insecure-Requests" : "1",
-        "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Sec-Fetch-Site" : "none",
-        "Sec-Fetch-Mode" : "navigate",
-        "Sec-Fetch-User" : "?1",
-        "Sec-Fetch-Dest" : "document",
-        "Accept-Encoding" : "gzip, deflate, br",
-        "Accept-Language" : "ko-KR,ko;q=0.9"
+        "Connection": "keep-alive",
+        "Cache-Control": "max-age=0",
+        "sec-ch-ua-mobile": "?0",
+        "DNT": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-User": "?1",
+        "Sec-Fetch-Dest": "document",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ko-KR,ko;q=0.9"
     }
 
 
@@ -34,9 +34,13 @@ def get_search_result(search_url, time_sleep_sec=0):
             response = session.get(search_url, headers=header_dc)
         soup = BeautifulSoup(response.text, "html.parser")  # 검색 결과 페이지
         element_list = soup.select("table.gall_list tr.ub-content")  # 한 페이지 전체 글 리스트
+        if len(element_list) == 0:  # 검색 결과는 광고글를 포함해서 최소 1개 이상이어야 한다. 없으면 다시 돌림
+            print("[검색 실패해서 반복합니다] [get_search_result()]")
+            element_list = get_search_result(search_url, time_sleep_sec+2)
     except Exception as e:
         print("[오류가 발생하여 반복합니다] [get_search_result()] ", e)
         element_list = get_search_result(search_url, time_sleep_sec+2)
+
     return element_list
 
 ###############################
