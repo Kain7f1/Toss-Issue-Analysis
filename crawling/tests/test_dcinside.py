d33a56dd09ab19a5421beecb10ea0839372aa701
Kain7f1 <Kain7f1>
2023-10-13 11:45:38 +0900

    test_dcinside.py 업그레이드 3
    - test_dcinside.py 업그레이드 3

--- crawling/tests/test_dcinside.py
+++ crawling/tests/test_dcinside.py
@@ -7,17 +7,17 @@ import utility_module as util
 #############################################################################
 #                                 << 설정값 >>
 keyword = "lg화학"       # 검색할 키워드
-gall_name = "부동산"    # 검색할 갤러리 선택하기
+gall_name = "코스피"    # 검색할 갤러리 선택하기
 
 # 검색할 키워드(keyword)의 블랙리스트
 # 목적에 맞지 않는 콘텐츠를 걸러내는 기능을 한다
 blacklist = {
       "에코": ["에코백", "에코페", "에코팰", "에코플", "에코마", "에코디", "에코랜", "에코스", "에코하", "아마존 에코"]
     , "에코프로": []
-    , "엘앤에프": []
+    , "엘앤에프": ["http"]
     , "에스엠": []
     , "카카오": []
-    , "lg화학": []
+    , "lg화학": ["http"]
     , "토스": ["토스트", "도리토스", "치토스", "멘토스", "셀토스", "키보토스", "프로토스", "테스토스", "토스테론"]
 }
 
@@ -25,22 +25,22 @@ blacklist = {
 gall_url = {
       "미국주식": "https://gall.dcinside.com/mgallery/board/lists?id=stockus"
     , "해외주식": "https://gall.dcinside.com/mgallery/board/lists/?id=tenbagger"
-    , "나스닥": "https://gall.dcinside.com/mgallery/board/lists?id=nasdaq"
     , "코스피": "https://gall.dcinside.com/mgallery/board/lists?id=kospi"
     , "국내선물옵션": "https://gall.dcinside.com/mini/board/lists/?id=koreafutures"
     , "실전주식투자": "https://gall.dcinside.com/mgallery/board/lists?id=jusik"
+    , "나스닥": "https://gall.dcinside.com/mgallery/board/lists?id=nasdaq"
     , "다우": "https://gall.dcinside.com/mgallery/board/lists?id=dow100"
     , "슨피": "https://gall.dcinside.com/mini/board/lists/?id=snp500"
     , "금융": "https://gall.dcinside.com/mgallery/board/lists?id=finance"
     , "증권": "https://gall.dcinside.com/mgallery/board/lists/?id=securities"
+    , "재테크": "https://gall.dcinside.com/mgallery/board/lists?id=jaetae"
+    , "부동산": "https://gall.dcinside.com/board/lists/?id=immovables"
+    , "신용카드": "https://gall.dcinside.com/board/lists/?id=creditcard"
+    , "체크카드": "https://gall.dcinside.com/mgallery/board/lists?id=checkcard"
     , "캠퍼스개미": "https://gall.dcinside.com/mgallery/board/lists?id=smow"
     , "에너지주식": "https://gall.dcinside.com/mini/board/lists/?id=energystock"
     , "초전도체": "https://gall.dcinside.com/board/lists/?id=superconductor"
-    , "재테크": "https://gall.dcinside.com/mgallery/board/lists?id=jaetae"
-    , "부동산": "https://gall.dcinside.com/board/lists/?id=immovables"
     , "편의점": "https://gall.dcinside.com/board/lists/?id=cs_new1"
-    , "신용카드": "https://gall.dcinside.com/board/lists/?id=creditcard"
-    , "체크카드": "https://gall.dcinside.com/mgallery/board/lists?id=checkcard"
     , "알뜰폰": "https://gall.dcinside.com/mgallery/board/lists?id=mvnogallery"
     , "SFF": "https://gall.dcinside.com/mgallery/board/lists?id=sff"
     , "토스": "https://gall.dcinside.com/mgallery/board/lists/?id=toss"
